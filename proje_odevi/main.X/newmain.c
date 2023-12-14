e0f79bd7ec4a05430eb00c598f824d9a30b39223
Furkan Taha Bademci <tahafurkanbademci@gmail.com>
2023-12-14 17:38:11 +0300

    LCD ekran çalışır halde, yön butonları eklenecek

--- proje_odevi/main.X/newmain.c
+++ proje_odevi/main.X/newmain.c
@@ -1,13 +1,14 @@
 #include <pic.h>
 
-// Bekleme makrosu
-#define BEKLE for (i = 0; i <= 1000; i++)
 #define RS RB0
 #define RW RB1
 #define E RB2
 
 // PIC konfigürasyon ayarları
-__CONFIG(FOSC_HS & WDTE_OFF & PWRTE_OFF & CP_OFF & BOREN_ON & LVP_OFF & CPD_OFF & WRT_OFF & DEBUG_OFF);
+__CONFIG(FOSC_HS &WDTE_OFF & PWRTE_OFF & CP_OFF & BOREN_ON & LVP_OFF & CPD_OFF & WRT_OFF & DEBUG_OFF);
+
+#define DUGME_SAG RA1 // Örneğin, sağ yön düğmesinin bağlı olduğu pin
+#define DUGME_SOL RA2 // Örneğin, sol yön düğmesinin bağlı olduğu pin
 
 // Fonksiyon prototipleri
 void adcOku();
@@ -15,19 +16,26 @@ void lcdBaslangic();
 void komut(unsigned char a);
 void veri(unsigned char b);
 void ekrandaGoster(unsigned char *metin);
+void bekle(int milisaniye);
 
-int i;
+#define _XTAL_FREQ 4000000 // 4 MHz kristal kullan?l?yorsa
 
 void main()
 {
     // Giriş/Çıkış tanımlamaları
-    TRISB = 0;   // Port B ve Port C Çıkış (LCD)
+    TRISB = 0; // Port B ve Port C Çıkış (LCD)
     TRISD = 0;
-    TRISA0 = 1;  // RA0 giriş (ADC)
+    TRISA0 = 1; // RA0 giriş (ADC)
 
     // LCD başlangıç ayarları
     lcdBaslangic();
-    ekrandaGoster("ADC Degeri :");
+    ekrandaGoster("Furkan T.Bademci");
+    komut(0xC0); // ikinci sat?ra ge�
+    ekrandaGoster("G210104016");
+    bekle(3000);
+
+    lcdBaslangic();
+    ekrandaGoster("ADC DEGERI :");
 
     while (1)
     {
@@ -43,7 +51,8 @@ void lcdBaslangic()
     komut(0x38); // 8-bit veri, 2 satır, 5x7 font
     komut(0x0C); // Ekranı aç, imleci kapat
     komut(0x06); // Yazıyı kaydır, imleci sabit tut
-    komut(0x80); // DDRAM adresini başlangıca ayarla
+    komut(0x80); // DDRAM adresini başlangıca ayarl
+    komut(0x01); // ekran? tem?zlea
 }
 
 void komut(unsigned char a)
@@ -53,7 +62,7 @@ void komut(unsigned char a)
     RS = 0;
     RW = 0;
     E = 1;
-    BEKLE;
+    bekle(10);
     E = 0;
 }
 
@@ -64,7 +73,7 @@ void veri(unsigned char b)
     RS = 1;
     RW = 0;
     E = 1;
-    BEKLE;
+    bekle(10);
     E = 0;
 }
 
@@ -85,7 +94,7 @@ void adcOku()
     ADCON1 = 0xc0; // Sağa dayalı
     ADCON0 = 0x85; // ADC açık, FOSC/64
     while (GO_nDONE)
-        ; // Dönüşüm bitene kadar bekle
+        ;                                  // Dönüşüm bitene kadar bekle
     adcDeger = ((ADRESH << 8) | (ADRESL)); // Sonucu sakla
     adcDeger = (adcDeger / 3) - 1;
 
@@ -94,4 +103,30 @@ void adcOku()
     veri(((adcDeger / 100) % 10) + 48);
     veri(((adcDeger / 10) % 10) + 48);
     veri((adcDeger % 10) + 48);
+
+    komut(0xC0); // ikinci sat?ra ge�
+    if (DUGME_SAG == 1)
+    {
+        ekrandaGoster("Sag");
+        while (DUGME_SAG == 1)
+            ;
+        __delay_ms(100); // Debouncing için kısa bir bekleme
+    }
+
+    if (DUGME_SOL == 1)
+    {
+        ekrandaGoster("Sol");
+        while (DUGME_SOL == 1)
+            ;
+        __delay_ms(100); // Debouncing için kısa bir bekleme
+    }
+}
+
+void bekle(int milisaniye)
+{
+    int i;
+    for (i = 0; i < milisaniye; i++)
+    {
+        __delay_ms(1);
+    }
 }
