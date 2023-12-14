9054f2e405222293d73751cc64f67119c8cfae0c
Furkan Taha Bademci <tahafurkanbademci@gmail.com>
2023-12-14 15:57:44 +0300

    initial commit

---
+++ proje_odevi/main.X/newmain.c
@@ -0,0 +1,97 @@
+#include <pic.h>
+
+// Bekleme makrosu
+#define BEKLE for (i = 0; i <= 1000; i++)
+#define RS RB0
+#define RW RB1
+#define E RB2
+
+// PIC konfigürasyon ayarları
+__CONFIG(FOSC_HS & WDTE_OFF & PWRTE_OFF & CP_OFF & BOREN_ON & LVP_OFF & CPD_OFF & WRT_OFF & DEBUG_OFF);
+
+// Fonksiyon prototipleri
+void adcOku();
+void lcdBaslangic();
+void komut(unsigned char a);
+void veri(unsigned char b);
+void ekrandaGoster(unsigned char *metin);
+
+int i;
+
+void main()
+{
+    // Giriş/Çıkış tanımlamaları
+    TRISB = 0;   // Port B ve Port C Çıkış (LCD)
+    TRISD = 0;
+    TRISA0 = 1;  // RA0 giriş (ADC)
+
+    // LCD başlangıç ayarları
+    lcdBaslangic();
+    ekrandaGoster("ADC Degeri :");
+
+    while (1)
+    {
+        // ADC değerini göstermek için LCD konumunu ayarla ve ADC okuma fonksiyonunu çağır
+        komut(0x8C);
+        adcOku();
+    }
+}
+
+void lcdBaslangic()
+{
+    // LCD başlangıç ayarları
+    komut(0x38); // 8-bit veri, 2 satır, 5x7 font
+    komut(0x0C); // Ekranı aç, imleci kapat
+    komut(0x06); // Yazıyı kaydır, imleci sabit tut
+    komut(0x80); // DDRAM adresini başlangıca ayarla
+}
+
+void komut(unsigned char a)
+{
+    // LCD'ye komut gönderme
+    PORTD = a;
+    RS = 0;
+    RW = 0;
+    E = 1;
+    BEKLE;
+    E = 0;
+}
+
+void veri(unsigned char b)
+{
+    // LCD'ye veri gönderme
+    PORTD = b;
+    RS = 1;
+    RW = 0;
+    E = 1;
+    BEKLE;
+    E = 0;
+}
+
+void ekrandaGoster(unsigned char *metin)
+{
+    // LCD'ye karakter dizisi gönderme
+    while (*metin)
+    {
+        veri(*metin++);
+    }
+}
+
+void adcOku()
+{
+    unsigned int adcDeger;
+
+    // ADC ayarları
+    ADCON1 = 0xc0; // Sağa dayalı
+    ADCON0 = 0x85; // ADC açık, FOSC/64
+    while (GO_nDONE)
+        ; // Dönüşüm bitene kadar bekle
+    adcDeger = ((ADRESH << 8) | (ADRESL)); // Sonucu sakla
+    adcDeger = (adcDeger / 3) - 1;
+
+    // ADC değerini LCD'ye gönderme
+    veri((adcDeger / 1000) + 48);
+    veri(((adcDeger / 100) % 10) + 48);
+    veri(((adcDeger / 10) % 10) + 48);
+    veri((adcDeger % 10) + 48);
+}
