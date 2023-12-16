2fd84c20978b497eb69e7b59086612cbf0b2b701
Furkan Taha Bademci <tahafurkanbademci@gmail.com>
2023-12-16 19:20:34 +0300

    add project

---
+++ ders_ornek_c_1.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ders_ornek_c_1.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ders_ornek_c_1.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=dersornekc1.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/dersornekc1.x.tar
